6321e27c3ce19fb890c07e1ae22e398b1b6a20a7
Asphalt 2.0 <ssnazarkar11@gmail.com>
2026-04-09 19:39:20 +0530

    Added Styling to the page

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,19 @@ function App() {
         content: (
           <div className='p-4 flex flex-col items-center justify-center'>
             <h2 className='text-xl font-bold mb-4'>Modal {prev.length + 1}</h2>
-            <p>Content for modal {prev.length + 1}.</p>
-            <button
-              className='mt-4 px-4 py-2 bg-blue-500 text-white rounded'
-              onClick={handleOpenModal}
-            >
-              Open Another Modal
-            </button>
+            <div className='text-center bg-gray-200 p-4 rounded-lg shadow-md shadow-black/30'>
+              <div>Click on Overlay to Close the Modal</div>
+              <div>OR</div>
+              <div>Drag Down to Close the Modal</div>
+            </div>
+            {prev.length < 4 && (
+              <button
+                className='mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg shadow-lg shadow-black/50'
+                onClick={handleOpenModal}
+              >
+                Open Another Modal
+              </button>
+            )}
           </div>
         ),
       },
@@ -43,13 +49,13 @@ function App() {
     <>
     <div className='flex flex-col items-center justify-center gap-8 p-4 h-[100dvh] bg-gradient-to-tl from-pink-300 to-violet-600'>
       <h1 className="text-3xl font-bold text-[rgb(93_15_155)] text-center">IOS like Modal Stacking App</h1>
-      <div className='text-center px-12'>
-        <h3 className="text-lg text-black-600">Stack multiple modals like iOS</h3>
-        <h3 className="text-md text-black-500">Drag down to close the top modal</h3>
-        <h3 className="text-sm text-black-400">Switch to Mobile View for better experience if you are on desktop</h3>
+      <div className='text-center px-4 bg-black bg-opacity-50 py-4 rounded-xl w-[80%] shadow-xl shadow-black/50'>
+        <div className="text-lg text-white">Stack multiple modals like iOS</div>
+        <div className="text-md text-white">Drag down to close the top modal</div>
+        <div className="text-sm text-white">Switch to Mobile View for better experience if you are on desktop</div>
       </div>
       <button
-      className='px-4 py-2 bg-green-500 text-white rounded'
+      className='px-4 py-2 bg-green-600 text-white rounded-lg shadow-lg shadow-black/50'
       onClick={handleOpenModal}
     >
       Open Modal
